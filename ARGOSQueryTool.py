93bd18e9151bf42c1e49dc343eb9a1d652adfbec
ectucker <42750861+ectucker@users.noreply.github.com>
2018-10-01 12:16:38 -0400

    Loops with while loop

--- ARGOSQueryTool.py
+++ ARGOSQueryTool.py
@@ -12,25 +12,25 @@ fileName = "SaraNoHeader.txt"
 fileObj = open(fileName, 'r')
 
 # Read in all lines in the text file into a list variable
-lineList = fileObj.readlines()
+lineString = fileObj.readline()
 
-# Closes the file object (now that we have all we need)
-fileObj.close()
+#Loop through everything using a while loop wite while followed by a condition
+# this loop evaluates by goin to the next line, if the next line doesn't exist then the command is false and it will stop running
+while lineString:
+    # Use the split command to parse the items in lineString into a list object
+    lineData = lineString.split("\t")
 
-# Extract the first line from the lineList
-lineString = lineList[0]
+    # Assign variables to specfic items in the list
+    recordID = lineData[0]              # ARGOS tracking record ID
+    obsDateTime = lineData[2]           # Observation date and time (combined)
+    obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+    obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+    obsLC = lineData[3]                 # Observation Location Class
+    obsLat = lineData[5]                # Observation Latitude
+    obsLon = lineData[6]                # Observation Longitude
 
-# Use the split command to parse the items in lineString into a list object
-lineData = lineString.split("\t")
+    # Print information to the user
+    print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLat,obsDate))
 
-# Assign variables to specfic items in the list
-recordID = lineData[0]              # ARGOS tracking record ID
-obsDateTime = lineData[2]           # Observation date and time (combined)
-obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
-obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
-obsLC = lineData[3]                 # Observation Location Class
-obsLat = lineData[5]                # Observation Latitude
-obsLon = lineData[6]                # Observation Longitude
-
-# Print information to the user
-print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLat,obsDate))
+    #go to next line
+    lineString = fileObj.readline()
